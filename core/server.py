95e9e1b0c38ba49622bc5338df7ac393c6d26026
Aaron Furtado <aaron.furtado20003@gmail.com>
2024-09-23 10:18:00 +0530

    added docker compose

--- core/server.py
+++ core/server.py
@@ -1,4 +1,7 @@
+import sys
 import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from flask import jsonify
 from marshmallow.exceptions import ValidationError
 from core import app
@@ -39,4 +42,4 @@ def handle_error(err):
     raise err
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
